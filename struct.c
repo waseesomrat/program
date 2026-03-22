0d4429cc9c6732dac53ccc35a1d761902f805974
wasi <waseesomrat8@gmail.com>
2026-03-22 18:08:22 +0600

    initial commit

---
+++ struct.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+struct student{
+    char name[200];
+    int age;
+};
+    int main() {
+        int n;
+        scanf("%d", &n);
+        struct student s[n];
+
+        for(int i = 0;i < n;i++){
+            printf("Enter name and age: ");
+            scanf("%s %d", s[i].name , &s[i].age);
+
+        }
+       for(int i = 0;i < n;i++){
+            printf("%s %d\n" , s[i].name , s[i].age);
+        }
+        return 0;
+}
+
+
