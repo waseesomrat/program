786beef3a77af4dbda3a64b3498af68d4192e36e
wasi <waseesomrat8@gmail.com>
2026-03-22 21:13:06 +0600

    initial commit

---
+++ struct_inside_function.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+struct student {
+
+    char name[20];
+    int age;
+};
+
+void printstudent(struct student s){
+
+    printf("%s %d\n",s.name,s.age);
+}
+
+int main() {
+
+struct student s1 = {"wasi",0};
+
+printstudent(s1);
+
+return 0;
+}
+
+
+
+
+
